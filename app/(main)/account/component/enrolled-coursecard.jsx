f7634b2c14579b36b39e583e793edc2fbd3d671b
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-02 20:33:18 +0600

    categories title show

--- app/(main)/account/component/enrolled-coursecard.jsx
+++ app/(main)/account/component/enrolled-coursecard.jsx
@@ -2,8 +2,14 @@ import { Badge } from "@/components/ui/badge";
 import { BookOpen } from "lucide-react";
 import Image from "next/image";
 
-export default function EnrolledCourseCard({ enrollment }) {
-  console.log(enrollment);
+import { getCategoryDetails } from "@/queries/categories";
+
+export default async function EnrolledCourseCard({ enrollment }) {
+  const courseCategory = await getCategoryDetails(
+    enrollment?.course?.category?._id,
+  );
+
+  console.log(courseCategory);
   return (
     <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
       <div className="relative w-full aspect-video rounded-md overflow-hidden">
@@ -18,7 +24,7 @@ export default function EnrolledCourseCard({ enrollment }) {
         <div className="text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2">
           {enrollment?.course?.title}
         </div>
-        <p className="text-xs text-muted-foreground">Development</p>
+        <p className="text-xs text-muted-foreground">{courseCategory?.title}</p>
         <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
           <div className="flex items-center gap-x-1 text-slate-500">
             <div>
