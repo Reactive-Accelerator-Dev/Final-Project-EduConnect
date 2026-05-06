060c9598b0337cd5f29fb57d5aa589f98eccbed4
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-06 17:01:34 +0600

    reviews show table

--- app/dashboard/courses/[courseId]/reviews/page.jsx
+++ app/dashboard/courses/[courseId]/reviews/page.jsx
@@ -6,20 +6,7 @@ import { getCourseDetails } from "@/queries/courses";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 
-const reviews = [
-  {
-    id: 1,
-    student: { name: "John Doe" },
-    review: "Nice Course, Thanks for the help",
-    rating: 5,
-  },
-  {
-    id: 1,
-    student: { name: "John Smilga" },
-    review: "Nice Course, Thanks for the help",
-    rating: 5,
-  },
-];
+
 const ReviewsPage = async ({ params: { courseId } }) => {
   const course = await getCourseDetails(courseId);
   const reviewsData = await getInstructorDashboardData(REVIEW_DATA);
@@ -27,7 +14,7 @@ const ReviewsPage = async ({ params: { courseId } }) => {
   return (
     <div className="p-6">
       <h2>{course?.title}</h2>
-      <DataTable columns={columns} data={reviews} />
+      <DataTable columns={columns} data={reviewsData} />
     </div>
   );
 };
