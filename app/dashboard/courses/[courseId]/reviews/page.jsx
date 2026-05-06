a1d2608af1c2ab9f36c4c0901d6e3077a7ca434f
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-06 17:35:51 +0600

    reviews data filter dfrent course

--- app/dashboard/courses/[courseId]/reviews/page.jsx
+++ app/dashboard/courses/[courseId]/reviews/page.jsx
@@ -9,12 +9,13 @@ import { DataTable } from "./_components/data-table";
 
 const ReviewsPage = async ({ params: { courseId } }) => {
   const course = await getCourseDetails(courseId);
-  const reviewsData = await getInstructorDashboardData(REVIEW_DATA);
-  console.log(reviewsData);
+  const reviewData = await getInstructorDashboardData(REVIEW_DATA);
+  const reviewDataForCourse = reviewData.filter(review => review?.courseId.toString() == courseId);
+//   console.log(reviewData);
   return (
     <div className="p-6">
       <h2>{course?.title}</h2>
-      <DataTable columns={columns} data={reviewsData} />
+      <DataTable columns={columns} data={reviewDataForCourse} />
     </div>
   );
 };
