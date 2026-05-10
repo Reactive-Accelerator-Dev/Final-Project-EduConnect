38d774ee0c10528a011851752249e58158859964
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-10 19:41:11 +0600

    module edit page daynamic route set

--- app/dashboard/courses/[courseId]/_components/module-form.jsx
+++ app/dashboard/courses/[courseId]/_components/module-form.jsx
@@ -83,7 +83,7 @@ export const ModulesForm = ({ initialData, courseId }) => {
   };
 
   const onEdit = (id) => {
-    router.push(`/dashboard/courses/1/modules/${1}`);
+    router.push(`/dashboard/courses/${courseId}/modules/${id}`);
   };
 
   return (
