55ff8a09cd013be78172ed4fac13b8cd1917a68c
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-12 19:15:25 +0600

    lesson actio from update

--- app/dashboard/courses/[courseId]/modules/[moduleId]/_components/lesson-action.jsx
+++ app/dashboard/courses/[courseId]/modules/[moduleId]/_components/lesson-action.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 export const LessonActions = ({ lesson, moduleId, onDelete }) => {
   const [action, setAction] = useState(null);
+  const [published, setPublished] = useState(lesson?.active);
   async function handleSubmit(event) {
     event.preventDefault();
     console.log(action);
@@ -18,7 +19,9 @@ export const LessonActions = ({ lesson, moduleId, onDelete }) => {
           variant="outline"
           size="sm"
           onClick={() => setAction("change-active")}
-        ></Button>
+        >
+          {published ? "Unpublish" : "Publish"}
+        </Button>
 
         <Button size="sm" onClick={() => setAction("delete")}>
           <Trash className="h-4 w-4" />
