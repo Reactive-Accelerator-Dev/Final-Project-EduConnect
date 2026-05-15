631628ec7b450b7bcae5eadf5bb5b3a5469b82c2
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-15 19:10:19 +0600

    updated module console log

--- app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
+++ app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
@@ -1,14 +1,41 @@
-"use client";
 import { CourseProgress } from "@/components/course-progress";
-import { useState } from "react";
+import { getCourseDetails } from "@/queries/courses";
 import { DownloadCertificate } from "./download-certificate";
 import { GiveReview } from "./give-review";
 import { SidebarModules } from "./sidebar-modules";
+import { Watch } from "@/model/watch-model";
+import { getLoggedInUser } from "@/lib/loggedin-user";
 
-export const CourseSidebar = () => {
-  const [isReviewModalOpen, setIsReviewModalOpen] = useState(false);
-  const isActive = true;
-  const isCompleted = true;
+export const CourseSidebar = async ({ courseId }) => {
+  const course = await getCourseDetails(courseId);
+  const loggedinUser = await getLoggedInUser();
+
+  const updatedModules = await Promise.all(
+    course?.modules.map(async (module) => {
+      const moduleId = module._id.toString();
+      const lessons = module?.lessonIds;
+
+      const updatedLessons = await Promise.all(
+        lessons.map(async (lesson) => {
+          const lessonId = lesson._id.toString();
+          const watch = await Watch.findOne({
+            lesson: lessonId,
+            module: moduleId,
+            user: loggedinUser.id,
+          }).lean();
+          if (watch?.state === "completed") {
+            console.log(`This lesson ${lesson.title} has completed`);
+            lesson.state = "completed";
+          }
+          return lesson;
+        }),
+      );
+      return module;
+    }),
+  );
+
+//   console.log("updatedModules",updatedModules);
+
   return (
     <>
       <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
