ffec2a7f9a85ca4b4f8e719bdc919b29c76dde6b
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-15 14:16:29 +0600

    lesson item defrent defrent componet set

---
+++ app/(main)/courses/[id]/lesson/_components/sidebar-lesson-items.jsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import { CheckCircle } from "lucide-react";
+import Link from "next/link";
+export default function SidebarLessonItems() {
+  const isActive = true;
+  const isCompleted = true;
+  return (
+    <div className="flex flex-col w-full gap-3">
+      {/* active and completed */}
+
+      <Link
+        href="#"
+        className={cn(
+          "flex items-center gap-x-2 text-slate-500 text-sm font-[500]  transition-all hover:text-slate-600 ",
+          isActive && "text-slate-700  hover:text-slate-700",
+          isCompleted && "text-emerald-700 hover:text-emerald-700",
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <CheckCircle
+            size={16}
+            className={cn(
+              "text-slate-500",
+              isActive && "text-slate-700",
+              isCompleted && "text-emerald-700",
+            )}
+          />
+          Introduction
+        </div>
+      </Link>
+    </div>
+  );
+}
---
+++ app/(main)/courses/[id]/lesson/_components/sidebar-lessons.jsx
@@ -0,0 +1,9 @@
+import { AccordionContent } from "@/components/ui/accordion";
+import SidebarLessonItems from "./sidebar-lesson-items";
+export default function SidebarLessons() {
+  return (
+    <AccordionContent>
+      <SidebarLessonItems />
+    </AccordionContent>
+  );
+}
