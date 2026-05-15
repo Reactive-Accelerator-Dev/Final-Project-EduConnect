6f5fe113ae71d64e5478f3f91389d6501994e0a1
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-15 20:10:43 +0600

    console log lesson video

--- app/(main)/courses/[id]/lesson/page.jsx
+++ app/(main)/courses/[id]/lesson/page.jsx
@@ -1,25 +1,46 @@
 import { Button } from "@/components/ui/button";
-import { VideoPlayer } from "./_components/video-player";
 import { Separator } from "@/components/ui/separator";
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/lib/convertData";
+import { getCourseDetails } from "@/queries/courses";
 import VideoDescription from "./_components/video-description";
+import { VideoPlayer } from "./_components/video-player";
+import { getLessonBySlug } from "@/queries/lessons";
 
-const Course = () => {
-	return (
-		<div>
-			<div className="flex flex-col max-w-4xl mx-auto pb-20">
-				<div className="p-4 w-full">
-					<VideoPlayer />
-				</div>
-				<div>
-					<div className="p-4 flex flex-col md:flex-row items-center justify-between">
-						<h2 className="text-2xl font-semibold mb-2">Introduction</h2>
-						<Button size="lg">Enroll</Button>
-					</div>
-					<Separator />
-					<VideoDescription />
-				</div>
-			</div>
-		</div>
-	);
+const Course = async ({ params: { id }, searchParams: { name, module } }) => {
+  const course = await getCourseDetails(id);
+  const allModules = replaceMongoIdInArray(course.modules).toSorted(
+    (a, b) => a.order - b.order,
+  );
+
+  const defaultLesson = replaceMongoIdInObject(
+    allModules[0]?.lessonIds?.toSorted((a, b) => a.order - b.order)[0],
+  );
+
+  const lessonToPlay = name ? await getLessonBySlug(name) : defaultLesson;
+
+  const defaultModule = module ?? allModules[0].slug;
+
+  console.log({ lessonToPlay });
+
+  return (
+    <div>
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4 w-full">
+          <VideoPlayer />
+        </div>
+        <div>
+          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">Introduction</h2>
+            <Button size="lg">Enroll</Button>
+          </div>
+          <Separator />
+          <VideoDescription />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Course;
