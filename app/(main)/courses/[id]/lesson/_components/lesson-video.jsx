5f784c0cbf4ab954e850dce5f4172922fd3ce03c
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-16 14:09:43 +0600

    video show

--- app/(main)/courses/[id]/lesson/_components/lesson-video.jsx
+++ app/(main)/courses/[id]/lesson/_components/lesson-video.jsx
@@ -1,7 +1,25 @@
-export default function LessonVideo() {
+"use client";
+
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player/youtube";
+
+export default function LessonVideo({ courseId, lesson, module }) {
+  const [hasWindow, setHasWindow] = useState(false);
+
+  useEffect(() => {
+    setHasWindow(true);
+  }, []);
+
   return (
-    <div>
-      <h1>lesson-video Component </h1>
-    </div>
+    <>
+      {hasWindow && (
+        <ReactPlayer
+          url={lesson?.video_url}
+          width="100%"
+          height="470px"
+          controls
+        />
+      )}
+    </>
   );
 }
