4e43320168c9f97642e6ed55097debc33a81e815
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-18 20:22:36 +0600

    certificate user info configration

--- app/(main)/courses/[id]/lesson/_components/download-certificate.jsx
+++ app/(main)/courses/[id]/lesson/_components/download-certificate.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button } from "@/components/ui/button";
 
 export const DownloadCertificate = ({ courseId, totalProgress }) => {
--- app/api/certificate/route.js
+++ app/api/certificate/route.js
@@ -1,3 +1,6 @@
+import { getLoggedInUser } from "@/lib/loggedin-user";
+import { getCourseDetails } from "@/queries/courses";
+
 // Fetch custom fonts
 const kalamFontUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/fonts/kalam/Kalam-Regular.ttf`;
 const kalamFontBytes = await fetch(kalamFontUrl).then((res) =>
@@ -13,3 +16,40 @@ const montserratFontUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/fonts/montserrat/
 const montserratFontBytes = await fetch(montserratFontUrl).then((res) =>
   res.arrayBuffer(),
 );
+
+export async function GET(request) {
+  try {
+    /* -----------------
+     *
+     * Configuratios
+     *
+     *-------------------*/
+    const searchParams = request.nextUrl.searchParams;
+    const courseId = searchParams.get("courseId");
+    const course = await getCourseDetails(courseId);
+    const loggedInUser = await getLoggedInUser();
+
+    const report = await getAReport({
+      course: courseId,
+      student: loggedInUser.id,
+    });
+    console.log(report?.completion_date);
+    const completionDate = report?.completion_date
+      ? formatMyDate(report?.completion_date)
+      : formatMyDate(Date.now());
+    console.log(completionDate);
+
+    const completionInfo = {
+      name: `${loggedInUser?.firstName} ${loggedInUser?.lastName}`,
+      completionDate: completionDate,
+      courseName: course.title,
+      instructor: `${course?.instructor?.firstName} ${course?.instructor?.lastName}`,
+      instructorDesignation: `${course?.instructor?.designation}`,
+      sign: "/sign.png",
+    };
+
+    console.log(completionInfo);
+  } catch (error) {
+    console.log(error);
+  }
+}
