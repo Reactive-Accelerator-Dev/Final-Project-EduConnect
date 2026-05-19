64eab0c7542113a9c1c37e6672c9d70d95a1ca2e
mahmudtech <mahmud.irfan.dev@gmail.com>
2026-05-19 16:46:19 +0600

    quiz data show

--- app/(main)/courses/[id]/lesson/_components/quiz-modal.jsx
+++ app/(main)/courses/[id]/lesson/_components/quiz-modal.jsx
@@ -28,7 +28,7 @@ function QuizModal({ courseId, quizSetId, quizzes, open, setOpen }) {
         <DialogContent className="sm:max-w-[95%] block">
           <div className="pb-4 border-b border-border text-sm">
             <span className="text-success inline-block mr-1">
-              {quizIndex + 1} / 2
+              {quizIndex + 1} / {quizzes.length}
             </span>{" "}
             টি প্রশ্ন
           </div>
@@ -48,8 +48,7 @@ function QuizModal({ courseId, quizSetId, quizzes, open, setOpen }) {
                   d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"
                 ></path>
               </svg>{" "}
-              Reactive Accelerator: Quiz 4.16 - রিয়্যাক্ট কম্পোনেন্টের
-              লাইফসাইকেল কোনটি বা কোনগুলো ?
+              {quizzes[quizIndex].title}
             </h3>
             <span className="text-[10px] block text-end">
               <svg
@@ -67,8 +66,7 @@ function QuizModal({ courseId, quizSetId, quizzes, open, setOpen }) {
                 <path d="M10 12h-4v-1h1v-3h-1v-1h3v4h1z"></path>
                 <path d="M8 0c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM8 14.5c-3.59 0-6.5-2.91-6.5-6.5s2.91-6.5 6.5-6.5 6.5 2.91 6.5 6.5-2.91 6.5-6.5 6.5z"></path>
               </svg>{" "}
-              একটি প্রশ্নের একাধিক উত্তর হতে পারে & ভুল সিলেকশনে কোন নেগেটিভ
-              মার্কিং নেই
+              There is no negative marking.
             </span>
           </div>
           <div className="grid md:grid-cols-2 gap-5 mb-6">
